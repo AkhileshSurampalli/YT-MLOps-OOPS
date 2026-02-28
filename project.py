2c2259066cf72f750bb2408d9f558e10698e7d3b
Akhilesh <surampalliakhilesh19@gmail.com>
2026-02-28 11:59:28 +0100

    sign up and sign in feature add in

--- project.py
+++ project.py
@@ -15,14 +15,37 @@ class chatbook:
         
 
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
+    
+    def signup(self):
+        email = input("Please enter your email -> ")
+        password = input("Please enter your password here ->")
+        self.username = email
+        self.password = password
+        print("You have signed up successfully")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password =='':
+            print('please sign up first by pressing 1 in the main menu')
+        else:
+            uname = input("Please enter your email/uname here -> ")
+            password = input("Please enter your password here: ")
+            if self.username==uname and self.password==password:
+                print("You have signed in successfully")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials")
+        print("\n")
+        self.menu()
 
 chatbook_obj = chatbook()
